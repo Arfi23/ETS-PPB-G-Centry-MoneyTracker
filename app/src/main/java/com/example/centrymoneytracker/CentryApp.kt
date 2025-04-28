107a45cf314ed542563494d205451d5a521fec7d
Arfi23 <arfifaizilraushani@gmail.com>
2025-04-28 22:55:35 +0700

    Add the transactionViewModel parameter to the home and add_transaction respectively

--- app/src/main/java/com/example/centrymoneytracker/CentryApp.kt
+++ app/src/main/java/com/example/centrymoneytracker/CentryApp.kt
@@ -1,33 +1,25 @@
 package com.example.centrymoneytracker
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.centrymoneytracker.ui.theme.CentryMoneyTrackerTheme
+import androidx.compose.runtime.remember
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.centrymoneytracker.ui.screens.HomeScreen
 import com.example.centrymoneytracker.ui.screens.AddTransactionScreen
+import com.example.centrymoneytracker.viewmodel.TransactionViewModel
 
 @Composable
 fun CentryApp() {
     val navController = rememberNavController()
+    val transactionViewModel = remember { TransactionViewModel() }
 
     NavHost(navController = navController, startDestination = "home") {
         composable("home") {
-            HomeScreen(navController)
+            HomeScreen(navController, transactionViewModel)
         }
         composable("add_transaction") {
-            AddTransactionScreen(navController)
+            AddTransactionScreen(navController, transactionViewModel)
         }
     }
 }
